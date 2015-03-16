065b97f3bba0a05b6d6794b05a1f2b5187d5c603
changhai <changhai@baidu.com>
2015-03-16 18:31:52 +0800

    to 1.1.2-SNAPSHOT

--- project/build.scala
+++ project/build.scala
@@ -3,7 +3,7 @@ import sbt.Keys._
 
 object build extends Build {
   val Organization = "com.baidu"
-  val Version = "1.1.1"
+  val Version = "1.1.2-SNAPSHOT"
   val ScalaVersion = "2.11.5"
   val ScalatraVersion = "2.3.6"
   val SlickVersion = "2.1.0"
