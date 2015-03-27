aef53dba250e2a26842e3e57049e9ec0746dbf30
changhai <changhai@baidu.com>
2015-03-27 12:35:42 +0800

    rel 1.1.6, support default config, add stargate reference.conf

--- smart-core/src/main/scala/com/baidu/smart/core/ConfigSupport.scala
+++ smart-core/src/main/scala/com/baidu/smart/core/ConfigSupport.scala
@@ -1,51 +1,55 @@
 package com.baidu.smart.core
 
-import com.typesafe.config.{ConfigException, ConfigFactory}
+import com.typesafe.config.{Config, ConfigException, ConfigFactory}
 
 /**
  * Created by changhai on 14/12/4.
  */
 object ConfigSupport {
+  val rootCfg = ConfigFactory.load()
   // support switch profile
-  val cfg = {
-    val c = ConfigFactory.load()
-    if (c.hasPath("profile")) c.getConfig(c.getString("profile")) else c
-  }
+  val cfg = if (rootCfg.hasPath("profile")) rootCfg.getConfig(rootCfg.getString("profile")) else rootCfg
 }
 
 trait ConfigSupport {
   // export cfg
+  val rootCfg = ConfigSupport.rootCfg
   val cfg = ConfigSupport.cfg
 
-  def hasPath(path: String) = cfg.hasPath(path)
+  def hasPath(path: String) = cfg.hasPath(path) || rootCfg.hasPath(path)
 
   def getOr[T](path: String, f: String => T, default: => T) = try f(path) catch {
     case e: Throwable => default
   }
 
-  def get[T](path: String, f: String => T) = try f(path) catch {
-    case e: ConfigException.Null => null.asInstanceOf[T]
+  def get[T](path: String, f: (Config, String) => T) = {
+    try f(cfg, path) catch {
+      case e: ConfigException.Null => null.asInstanceOf[T]
+      case e: Throwable => try f(rootCfg, path) catch {
+        case e: ConfigException.Null => null.asInstanceOf[T]
+      }
+    }
   }
 
-  def getString(path: String) = get(path, cfg.getString)
+  def getString(path: String) = get(path, (c, p) => c.getString(p))
 
   def getStringOr(path: String, default: => String) = getOr(path, getString, default)
 
-  def getLong(path: String) = get(path, cfg.getLong)
+  def getLong(path: String) = get(path, (c, p) => c.getLong(p))
 
   def getLongOr(path: String, default: => Long) = getOr(path, getLong, default)
 
-  def getDouble(path: String) = get(path, cfg.getDouble)
+  def getDouble(path: String) = get(path, (c, p) => c.getDouble(p))
 
   def getDoubleOr(path: String, default: => Double) = getOr(path, getDouble, default)
 
-  def getInt(path: String) = get(path, cfg.getInt)
+  def getInt(path: String) = get(path, (c, p) => c.getInt(p))
 
   def getIntOr(path: String, default: => Int) = getOr(path, getInt, default)
 
-  def getBoolean(path: String) = get(path, cfg.getBoolean)
+  def getBoolean(path: String) = get(path, (c, p) => c.getBoolean(p))
 
   def getBooleanOr(path: String, default: => Boolean) = getOr(path, getBoolean, default)
 
-  def getConfig(path: String) = get(path, cfg.getConfig)
+  def getConfig(path: String) = get(path, (c, p) => c.getConfig(p))
 }
